fc6e72395aa911da3b04111c5a1e1fec9940466d
leandro_tercette <leandro_tercette@gmail.com>
2022-11-26 15:48:51 -0300

    manupulating element classes

---
+++ shared.js
@@ -0,0 +1,41 @@
+var backdrop = document.querySelector('.backdrop');
+var modal = document.querySelector('.modal');
+var selectPlanButtons = document.querySelectorAll('.plan button');
+var noButton = document.querySelector('.modal__action--negative');
+var toggleButton = document.querySelector('.toggle-button');
+var mobileNav = document.querySelector('.mobile-nav');
+
+//console.log(selectPlanButtons);
+
+for (var i = 0; i < selectPlanButtons.length; i++) {
+    selectPlanButtons[i].addEventListener('click', function() {
+        /* modal.style.display = 'block';
+        backdrop.style.display = 'block'; */
+        //modal.className = "open"; //This will actually overwrite the complete list
+        modal.classList.add('open');
+        backdrop.classList.add('open')
+    });
+}
+
+backdrop.addEventListener('click', function() {
+    /* mobileNav.style.display = "none"; */
+    mobileNav.classList.remove('open');
+    closeModal();
+});
+
+noButton.addEventListener('click', closeModal);
+
+function closeModal() {
+    /* backdrop.style.display = 'none';
+    modal.style.display = 'none'; */
+    modal.classList.remove('open');
+    backdrop.classList.remove('open');
+} 
+
+toggleButton.addEventListener('click', function() {
+   /*  mobileNav.style.display = "block";
+    backdrop.style.display = "block"; */
+    mobileNav.classList.add('open');
+    backdrop.classList.add('open');
+})
+
